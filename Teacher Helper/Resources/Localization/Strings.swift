86d25df54e0a0fa22baaa3bd84e92ae29f4d80c3
Nodirbek Khudoyberdiev <kh.nodirbeek@gmail.com>
2023-12-10 01:07:02 +0500

    added strings

---
+++ Teacher Helper/Resources/Localization/Strings.swift
@@ -0,0 +1,13 @@
+//
+//  Strings.swift
+//  Teacher Helper
+//
+//  Created by Nodirbek Khudoyberdiev on 10/12/23.
+//
+
+import Foundation
+
+enum Strings: String {
+    case noInternetConnection = "no_internet_connection"
+    case wrongPinOrUsername = "wrong_pin_or_username"
+}
